8bb8ca51590a7a41ac639eba5f6f97e6af06f94e
Y2Nk4 <1147678303@qq.com>
2020-05-15 18:26:57 -0400

    更新
    · 添加 ENV 全局变量
    · 添加错误捕获
    · 优化 response
    · 替换 koa-logger 为 koa-log4

---
+++ server/middlewares/accessLogger.js
@@ -0,0 +1,81 @@
+let log4js = require('koa-log4'),
+    logger = log4js.getLogger('middleware'),
+    // eslint-disable-next-line no-unused-vars
+    colors = require('colors'),
+    Counter = require('passthrough-counter'),
+    humanize = require('humanize-number'),
+    bytes = require('bytes')
+
+const colorCodes = {
+    5: 'red',
+    4: 'yellow',
+    3: 'cyan',
+    2: 'green',
+    1: 'green',
+    0: 'yellow'
+}
+
+export default async function (ctx, next) {
+    let start = new Date()
+    logger.info(`${'client -->'.gray} %s %s`, ctx.method, ctx.originalUrl.gray)
+
+    return next().then(function () {
+        // calculate the length of a streaming response
+        // by intercepting the stream with a counter.
+        // only necessary if a content-length header is currently not set.
+        const length = ctx.response.length
+        const body = ctx.body
+        let counter
+        if (length == null && body && body.readable) {
+            ctx.body = body
+                .pipe(counter = Counter())
+                .on('error', ctx.onerror)
+        }
+
+        // log when the response is finished or closed,
+        // whichever happens first.
+        const res = ctx.res
+
+        const onfinish = done.bind(null, 'finish')
+        const onclose = done.bind(null, 'close')
+
+        res.once('finish', onfinish)
+        res.once('close', onclose)
+
+        function done (event) {
+            res.removeListener('finish', onfinish)
+            res.removeListener('close', onclose)
+
+            let showLength = '',
+                len = counter ? counter.length : length
+            if (~[204, 205, 304].indexOf(ctx.status)) {
+                showLength = ''
+            } else if (len == null) {
+                showLength = '-'
+            } else {
+                showLength = bytes(len)
+            }
+
+            logger.info(`${'client <--'.gray} %s %s %s - %s Size:%s`, ctx.method, ctx.url.gray, formatStatusCode(ctx.status), formatTime(start).green, showLength.green)
+        }
+    }, function (err) {
+        // log uncaught downstream errors
+        logger.info(`${'client <--'.gray} %s %s - %s`, ctx.status, ctx.url.red, formatTime(start).toString().green, err)
+        throw err
+    })
+}
+
+function formatStatusCode (status) {
+    status = status.toString()
+    let s = status / 100 | 0,
+        color = colorCodes[s]
+
+    return status[color]
+}
+
+function formatTime (start) {
+    const delta = new Date() - start
+    return humanize(delta < 10000
+        ? delta + 'ms'
+        : Math.round(delta / 1000) + 's')
+}
